ca8182af1ca7bfd519dd535e362614bbc034c44b
Dion Earle <z5205292@unsw.edu.au>
2018-06-01 20:46:38 +1000

    give on Fri  1 Jun 20:46:38 AEST 2018

--- fruit_bot.c
+++ fruit_bot.c
@@ -320,7 +320,8 @@ int best_location(struct bot *b, char destination_type[MAX_NAME_CHARS], int valu
         }
 
         // If this is the right location type, record price and east length
-        if ((strcmp(cur_east->fruit, destination_type) == 0) && (skip_east == 0)) {
+        if ((strcmp(cur_east->fruit, destination_type) == 0) && (skip_east == 0)
+        && east_len < (4 * b->maximum_move)) {
             prices_east_array[i] = cur_east->price;
             distance_east_array[i] = east_len;
             i++;
@@ -350,7 +351,8 @@ int best_location(struct bot *b, char destination_type[MAX_NAME_CHARS], int valu
         }
 
         // If this is the right location type, record price and west length
-        if ((strcmp(cur_west->fruit, destination_type) == 0) && (skip_west == 0)) {
+        if ((strcmp(cur_west->fruit, destination_type) == 0) && (skip_west == 0)
+        && west_len < (4 * b->maximum_move)) {
             prices_west_array[j] = cur_west->price;
             distance_west_array[j] = west_len;
             j++;
